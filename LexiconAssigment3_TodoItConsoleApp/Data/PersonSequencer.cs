3e60a61e3e00d0313eaaca9183208f93bca446d4
Muzdalifa <a.muzdalifa27@gmail.com>
2021-06-30 00:25:30 +0200

    Create PersonSequencer class and test it.

---
+++ LexiconAssigment3_TodoItConsoleApp/Data/PersonSequencer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LexiconAssigment3_TodoItConsoleApp.Data
+{
+    public class PersonSequencer
+    {
+        //Set initial personId value to 0
+        private static int personId = 0;
+        public static int PersonId
+        {
+            private set
+            {   
+                //set the next personId
+                personId = NextPersonId();
+            }
+            get
+            {
+                return personId;
+            }
+        }
+
+        //increment personId and return the next personId value.
+        public static int NextPersonId()
+        {
+            return ++personId;
+        }
+
+        public static int Reset()
+        {
+            return personId = 0;
+        }
+
+
+ 
+    }
+}
--- LexiconAssigment3_TodoItConsoleApp/Program.cs
+++ LexiconAssigment3_TodoItConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using LexiconAssigment3_TodoItConsoleApp.Model;
+﻿using LexiconAssigment3_TodoItConsoleApp.Data;
+using LexiconAssigment3_TodoItConsoleApp.Model;
 using System;
 
 namespace LexiconAssigment3_TodoItConsoleApp
@@ -13,14 +14,21 @@ namespace LexiconAssigment3_TodoItConsoleApp
 
             //Person person1 = new Person("", "");
             //Console.WriteLine(person1.PrintInfo());
-            Person person2 = new Person("Sune", "Andersson");
-            Console.WriteLine(person2.PrintInfo());
+            //Person person2 = new Person("Sune", "Andersson");
+            //Console.WriteLine(person2.PrintInfo());
 
-            Console.WriteLine("TODO:");
-            Todo todo = new Todo(1, "Going supermarket");
-            Console.WriteLine(todo.PrintInfo());
-            Todo todo2 = new Todo(2, "Cooking food");
-            Console.WriteLine(todo2.PrintInfo());
+            //Console.WriteLine("TODO:");
+            //Todo todo = new Todo(1, "Going supermarket");
+            //Console.WriteLine(todo.PrintInfo());
+            //Todo todo2 = new Todo(2, "Cooking food");
+            //Console.WriteLine(todo2.PrintInfo());
+
+            //PersonSequencer 
+            Console.WriteLine(PersonSequencer.PersonId);
+            Console.WriteLine(PersonSequencer.NextPersonId());
+            Console.WriteLine(PersonSequencer.NextPersonId());
+            Console.WriteLine(PersonSequencer.Reset());
+            Console.WriteLine(PersonSequencer.NextPersonId());
 
 
 
