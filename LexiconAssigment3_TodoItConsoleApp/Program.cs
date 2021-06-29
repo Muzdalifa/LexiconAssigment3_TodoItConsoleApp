15dd8381331de1d7f0d708141db0690d4f216d66
Muzdalifa <a.muzdalifa27@gmail.com>
2021-06-29 08:09:53 +0200

    Add project files.

---
+++ LexiconAssigment3_TodoItConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LexiconAssigment3_TodoItConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
